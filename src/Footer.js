14baa1d510077400a4f3ee9c55b7e2b144dffffd
mannuru <rmannuru@dsservices.com>
2023-02-13 23:31:16 -0500

    Added assets and Semantic Elements

---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+import FooterLogo from './assets/logo2.png';
+const Footer = () => {
+  return <footer>
+    <img className="footer-img" src={FooterLogo} alt="Footer Logo" />
+  </footer>;
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import Nav from './Nav';
+import Logo from './assets/logo1.svg';
+const Header = () => {
+  return <header>
+    <img className="header-logo" src={Logo} alt="logo" />
+    <Nav />
+  </header>;
+}
+
+export default Header;

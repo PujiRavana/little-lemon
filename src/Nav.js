4bfe14248fac11ad91cc1ad9b38073d680effdaf
PujiRavana <pujitha.ravana@gmail.com>
2023-02-18 20:48:10 -0500

    Navigation link Changes

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,12 @@
 const Nav = () => {
   return <nav>
     <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Menu</a></li>
-      <li><a href="#">Reservations</a></li>
-      <li><a href="#">Order Online</a></li>
-      <li><a href="#">Login</a></li>
+      <li><a href="/home">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/menu">Menu</a></li>
+      <li><a href="/reservations">Reservations</a></li>
+      <li><a href="/orderonline">Order Online</a></li>
+      <li><a href="/Login">Login</a></li>
     </ul>
   </nav>;
 };
